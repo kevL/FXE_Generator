dbe9cc689ffd00f326573d79f82be30af8373988
kevL <k3v1s73n@gmail.com>
2020-05-15 12:35:10 -0600

    2.2.2.0 - update ver

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 #endif
 [assembly: AssemblyCompany("0100010 Industries")]
 [assembly: AssemblyProduct("nwn2 FXE Generator/LipSyncer")]
-[assembly: AssemblyCopyright("2020 may 9")]
+[assembly: AssemblyCopyright("2020 may 15")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,5 +19,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("6cce54fb-ce5e-4384-bd3b-0a88b226ee66")]
 
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.2.2.0")]
+[assembly: AssemblyFileVersion("2.2.2.0")]
